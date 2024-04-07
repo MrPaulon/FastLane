cfdc5b7fec5c9ab2db9bcac490c3d2aa14e83198
Paul <76972320+MrPaulon@users.noreply.github.com>
2024-04-08 00:50:28 +0200

    Create install base.sh

---
+++ install base.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo wget https://github.com/compujuckel/AssettoServer/releases/download/v0.0.54/assetto-server-linux-x64.tar.gz
+sudo tar -xvf assetto-server-linux-x64.tar.gz
+sudo rm assetto-server-linux-x64.tar.gz
